7d6a71685da1dd4c7aa317ffc5829415a79cd5ff
NatsukiYoshioka <bonenatsuki@gmail.com>
2024-02-03 19:25:32 +0900

    Set game UI

--- GameFile/GameScene.cpp
+++ GameFile/GameScene.cpp
@@ -1,10 +1,17 @@
 #include"BaseScene.h"
+#include"MenuScene.h"
+#include"Font.h"
+#include"common.h"
 #include"DxLib.h"
 #include "GameScene.h"
 
-GameScene::GameScene()
+const unsigned int GameScene::countColor = GetColor(207, 255, 220);
+
+GameScene::GameScene():
+	startCount(initializeNum),
+	nowCount(static_cast<float>(initializeNum))
 {
-	
+	startCount = GetNowCount();
 }
 
 GameScene::~GameScene()
@@ -14,10 +21,16 @@ GameScene::~GameScene()
 
 void GameScene::Update()
 {
-	
+	nowCount = maxCount - static_cast<float>((GetNowCount() - startCount) / secondFrame);
 }
 
 void GameScene::Draw()
 {
 	//DrawString(stringX, stringY, inGameString, sceneStringColor);
+	if (MenuScene::GetChoose() == MenuScene::SELECT::PLAY)
+	{
+		ChangeFont(timeFontName, DX_CHARSET_DEFAULT);
+		SetFontSize(countSize);
+		DrawFormatStringF(countX, countY, countColor, "%.1f", nowCount);
+	}
 }
--- GameFile/GameScene.h
+++ GameFile/GameScene.h
@@ -12,5 +12,12 @@ public:
 	void Draw();
 private:
 	static constexpr const char* inGameString = "INGAME";
+	static constexpr int countSize = 112;						//カウントの文字サイズ
+	static constexpr float countX = 1275.0f;					//カウントの文字X座標
+	static constexpr float countY = 200.0f;						//カウントの文字Y座標
+	static const unsigned int countColor;						//カウントの文字カラー
+	static constexpr float maxCount = 100.0f;					//制限時間
+	int startCount;												//最初のカウント時間
+	float nowCount;												//残り制限時間
 };
 
